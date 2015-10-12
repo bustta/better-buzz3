a66623c0cd63106ca2428424d6ba6a865e715c85
bustta <bustta80980@gmail.com>
2015-10-12 16:27:52 +0800

    use pg to replace default sqlite and add requirements.txt

--- betterbuzz3/settings.py
+++ betterbuzz3/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'betterbuzz3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbbuzz3',
+        'USER': os.environ['PG_USER'],
+        'PASSWORD': os.environ['PG_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
